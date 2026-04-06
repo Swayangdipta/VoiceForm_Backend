2b24b58347c2edbc308efaa62c3de7dea8173397
Swayangdipta Das <31970602+Swayangdipta@users.noreply.github.com>
2026-04-06 16:17:47 +0530

    Updated DB Conn config

--- config/db.js
+++ config/db.js
@@ -6,9 +6,6 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: true
-  }
 });
 
 module.exports = pool;
